a61fef954e25aa479eb5f0c216f5200cb4bfc302
liuyang <2330053403@qq.com>
2021-12-28 14:05:09 +0800

    forth commit

---
+++ code/26-JS模块化解析/26.5ESModule/06-内部原理/foo.js
@@ -0,0 +1,1 @@
+// console.log('foo被执行了')
---
+++ code/26-JS模块化解析/26.5ESModule/06-内部原理/main.js
@@ -0,0 +1,8 @@
+// console.log(module)
+
+// delete module.loaded
+
+// require('./foo.js')
+// require('./foo.js')
+// require('./foo.js')
+// console.log(module)
