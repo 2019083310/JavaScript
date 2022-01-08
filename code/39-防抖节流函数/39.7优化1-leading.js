39b9e79e32077e19aaa9383ae5c0ffb2e77fd45e
liuyang <2330053403@qq.com>
2022-01-08 18:54:15 +0800

    sixth commit

--- code/39-防抖节流函数/39.7优化1-leading.js
+++ code/39-防抖节流函数/39.7优化1-leading.js
@@ -1,8 +1,13 @@
-function throttle(fn,interval){
+function throttle(fn,interval,options={leading:true,trailing:false}){
   let lastTime=0
+  const {leading,trailing}=options
 
   function _throttle(){
     const nowTime=new Date().getTime()
+
+    // leading优化
+    if(!leading&&!lastTime) lastTime=nowTime
+    
     let remainTime=interval-(nowTime-lastTime)
     if(remainTime<=0){
       fn()
