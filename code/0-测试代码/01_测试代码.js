425ea7a20f0a199cfee67ac6c5de964a91fdedf5
2019083310 <2330053403@qq.com>
2022-02-03 15:18:46 +0800

    eight commit

--- code/0-测试代码/01_测试代码.js
+++ code/0-测试代码/01_测试代码.js
@@ -9,3 +9,21 @@ var obj = {
 
 var nums = [1,2,3]
 nums.forEach(foo,obj)
+
+
+// splice方法
+// 1.删除操作
+const arr=[1,2,3,4,5]
+arr.splice(0,1)
+console.log(arr)
+
+// 2.插值操作
+arr.splice(0,0,1)
+console.log(arr)
+
+arr.splice(0,0,1,2)
+console.log(arr)
+
+// 3.修改操作
+arr.splice(0,1,3)
+console.log(arr)
